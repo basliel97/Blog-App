84defc58206cdf8a396d459bbc474c07469cff6f
Basliel Getu <baslielgetu97@example.com>
2025-07-07 09:49:06 +0300

    connected to the backend depolyed on render

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import useAuthStore from "../store/authStore";
 
 // API Configuration
 const API_CONFIG = {
-  baseURL: "http://localhost:3000",
+  baseURL: "https://blog-api-1-r23t.onrender.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
